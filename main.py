5dc8c9d3b0ef971e5f4aec993563fa726d5163c3
haseab <hullah3000@gmail.com>
2020-08-24 23:09:40 -0400

    Added functions (possibly faulty) ;)

--- main.py
+++ main.py
@@ -6,15 +6,3 @@ def get_range(dataframe, start_date='2018-4-1', end_date='2018-5-1'):
     common = dataframe[dataframe['Datetime'].isin(date_list)]
     return common
 
-
-def timeframe_setter(dataframe, timeFrame=77):
-    """Converts minute candlestick data into the timeframe(tf) of choice"""
-    df = dataframe.iloc[::timeFrame].copy()
-    df['High'] = np.array(
-        [max(dataframe['High'][i:timeFrame + i]) for i in range(0, len(dataframe['High']), timeFrame)])
-    df['Low'] = np.array([min(dataframe['Low'][i:timeFrame + i]) for i in range(0, len(dataframe['Low']), timeFrame)])
-    df['Close'] = np.array([dataframe['Close'].iloc[timeFrame - 1 + i] for i in
-                            range(0, len(dataframe['Close']) - timeFrame + 1, timeFrame)])
-    return df
-    #returns foo
-    #ignore above, return bar
