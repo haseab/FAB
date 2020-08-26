5e20a62f1cf710eeceef7e5d26c17407b4841f16
haseab <hullah3000@gmail.com>
2020-08-26 07:25:52 -0400

    main.py turned into example.py, added sample data, separated strategy into a separate file, added some sample images

---
+++ py files/example.py
@@ -0,0 +1,14 @@
+from dataload import DataLoad
+from backtester import Backtester
+from bm_strategy import *
+import pandas as pd
+
+csvUrl = "bitcoin_data_2018_1_1_to_2018_5_1.csv"
+
+load1 = DataLoad()
+data = load1.load_csv(csvUrl)
+strategy = bm_strategy(load1, data,7,77,231,880,2354,77,"2018-01-01","2018-05-01")
+
+backtest_bm = Backtester(data,strategy)
+
+print(backtest_bm.trade_stats(strategy))
