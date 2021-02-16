1df6e69939c2de451c75bd878b03347695bf26a3
haseab <hullah3000@gmail.com>
2021-02-15 21:29:49 -0500

    add static method

--- src/helper.py
+++ src/helper.py
@@ -8,19 +8,23 @@ import numpy as np
 class Helper:
     """class that is filled with a bunch of helper methods needed to run the bot"""
 
+    @staticmethod
     def sig_fig(x: float, sig: int = 2) -> float:
         """ Rounds to the number of significant digits indicated"""
         return round(x, sig - math.ceil(math.log10(abs(x))))
 
+    @staticmethod
     def string_to_timestamp(date: str) -> int:
         """Converts String of form DD-MM-YY into millisecond timestamp"""
         return int(time.mktime(datetime.strptime(date, "%d/%m/%Y").timetuple())) * 1000
 
+    @staticmethod
     def into_dataframe(lst: list) -> pd.DataFrame:
         """Converts Binance response list into dataframe"""
         return pd.DataFrame(lst, columns=["Timestamp", "Open", "High", "Low", "Close", "Volume",
                                           "Timestamp_end", "", "", "", "", ""]).set_index("Timestamp")
 
+    @staticmethod
     def calculate_minute_disparity(df: pd.DataFrame, tf: int) -> float:
         """Calculates the difference (in minutes) of old the current dataframe is with respect to the live data"""
         # Getting the last date on your current dataset
@@ -31,6 +35,7 @@ class Helper:
         diff = (current_minute - last_minute - timedelta(minutes=tf)).seconds / 60
         return diff
 
+    @staticmethod
     def determine_candle_positions(max_candles_needed, tf):
         # Ex. The 231 MA needs 231 candles of data to work. We use 4 more candles for safety.
         max_candles_needed += 4
