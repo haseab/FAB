33139cfa025d3621dd4a591f7c1261646d1a369a
haseab <hullah3000@gmail.com>
2021-04-15 01:05:36 -0400

    Refactor

--- src/screener.py
+++ src/screener.py
@@ -3,11 +3,13 @@ import sqlalchemy
 from backtester import Backtester
 from fab_strategy import FabStrategy
 from trader import Trader
+from datetime import datetime
+from dataloader import _DataLoader
 
-class Screener():
+class Screener:
 
     def __init__(self):
-        self.engine = sqlalchemy.create_engine('mysql+pymysql://root:***REMOVED***@localhost:3306/sample')
+        self.loader = _DataLoader()
 
     def check_for_signals(self, df, strategy):
         strategy.load_data(df)
@@ -31,24 +33,35 @@ class Screener():
         return False, None, None
 
 
-    def screen(self):
-        df_metrics = pd.read_sql("SELECT * FROM metrics", self.engine).drop("id", axis=1).set_index(["symbol", "timeframe"])
+    def screen(self, trader):
+        cursor = self.loader.sql.conn.cursor()
 
-        screener_columns = ["Signal?", "How Recent?"] + list(df_metrics.columns)
-        screener = pd.DataFrame(columns=screener_columns).set_index(["symbol", "timeframe"])
+        """ Add 'side when setting index"""
+        df_metrics = self.loader.sql.SELECT(f"* FROM metrics", cursor).set_index(['symbol', 'tf', 'rule_no','metric_id']).astype(float)
+        partial_screener = pd.DataFrame(columns=['date', 'signal', 'how recent', 'metric_id'])
+        current_symbol, current_tf = None, None
+        signal = False
 
-        for symbol, tf in df_metrics.index:
-            df = pd.read_sql(f"SELECT * FROM candlesticks WHERE SYMBOL = {symbol} and TIMEFRAME = {tf}", self.engine)
+        for symbol, tf, rules, metric_id in df_metrics.index:
+            if symbol == current_symbol and tf == current_tf:
+                continue
+
+            print(f'checking: {symbol, tf} ')
+            df = trader.set_asset(symbol, tf, max_candles_needed=255)
+
             signal, x_many_minutes_ago, rule = self.check_for_signals(df, FabStrategy())
+            current_symbol, current_tf = symbol, tf
 
             if signal:
-                screener.append(df.loc[symbol, tf])
+                print('SIGNAL!!!')
+                date = str(datetime.now())[:10]
+                partial_screener.append(pd.DataFrame([[date, rule, x_many_minutes_ago, metric_id]],
+                                                    columns=['date', 'signal', 'how recent', 'metric_id']))
+                signal, x_many_minutes_ago= None, None
 
-                last_row = len(df) - 1
-                screener.loc[last_row, 'Signal?'] = rule
-                screener.loc[last_row, 'How Recent?'] = x_many_minutes_ago
+        master_screener = partial_screener.merge(df_metrics, on='metric_id')
 
-        return screener.sort_values("RRR")
+        return master_screener.sort_values("unrealized_rrr", ascending=False)
 
 
 
