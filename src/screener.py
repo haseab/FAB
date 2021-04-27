9d530d66ce41e046b2dc1136a284a157b0a58af7
haseab <hullah3000@gmail.com>
2021-04-26 23:26:32 -0400

    Create threading upgrade for fetching data

--- src/screener.py
+++ src/screener.py
@@ -3,128 +3,141 @@ import sqlalchemy
 from backtester import Backtester
 from fab_strategy import FabStrategy
 from trader import Trader
+import time
 from datetime import datetime, timedelta
 from dataloader import _DataLoader
 from IPython.display import display, clear_output
 from playsound import playsound
+from helper import Helper
+from concurrent.futures import ThreadPoolExecutor
 
 class Screener:
 
     def __init__(self):
         self.loader = _DataLoader()
         self.master_screener = pd.DataFrame()
+        self.strategy = FabStrategy()
 
     def check_for_signals(self, df, strategy, max_candle_history=10):
         strategy.load_data(df)
         strategy.update_moving_averages()
 
-        # list_of_enters = {'Rule1 Buy': strategy.rule_1_buy_enter,
-        #                  'Rule1 Short': strategy.rule_1_short_enter,
-        #                  'Rule2 Buy': strategy.rule_2_buy_enter,
-        #                  'Rule2 Short':strategy.rule_2_short_enter,
-        #                  'Rule3 Buy': strategy.rule_3_buy_enter,
-        #                  'Rule3 Short': strategy.rule_3_short_enter}
-        #
-        # list_of_exits = {'Rule1 Buy': strategy.rule_1_buy_exit,
-        #                  'Rule1 Short': strategy.rule_1_short_exit,
-        #                  'Rule2 Buy': strategy.rule_2_buy_stop,
-        #                  'Rule2 Short': strategy.rule_2_short_stop,
-        #                  'Rule3 Buy': strategy.rule_1_buy_exit,
-        #                  'Rule3 Short': strategy.rule_1_short_exit,
-        #                  }
-
         list_of_enters = {
-                         'Rule2 Buy': strategy.rule_2_buy_enter,
-                         'Rule2 Short':strategy.rule_2_short_enter,
-                        }
+                            ('Rule 1', 'Long'): strategy.rule_1_buy_enter,
+                            ('Rule 1', 'Short'): strategy.rule_1_short_enter,
+                            ('Rule 2', 'Long'):  strategy.rule_2_buy_enter,
+                            ('Rule 2', 'Short'): strategy.rule_2_short_enter,
+                            ('Rule 3', 'Long'): strategy.rule_3_buy_enter,
+                            ('Rule 3', 'Short'): strategy.rule_3_short_enter
+                          }
 
         list_of_exits = {
-                         'Rule2 Buy': strategy.rule_2_buy_stop,
-                         'Rule2 Short': strategy.rule_2_short_stop
-                         }
+                            ('Rule 1', 'Long'): strategy.rule_1_buy_exit,
+                            ('Rule 1', 'Short'): strategy.rule_1_short_exit,
+                            ('Rule 2', 'Long'):  strategy.rule_2_buy_stop,
+                            ('Rule 2', 'Short'): strategy.rule_2_short_stop,
+                            ('Rule 3', 'Long'): strategy.rule_1_buy_exit,
+                            ('Rule 3', 'Short'): strategy.rule_1_short_exit
+                          }
 
+        # list_of_enters = {
+        #                 ('Rule 2', 'Long'): strategy.rule_2_buy_enter,
+        #                 ('Rule 2', 'Short'): strategy.rule_2_short_enter,
+        #                 }
+        #
+        # list_of_exits = {
+        #                  ('Rule 2', 'Long'): strategy.rule_2_buy_stop,
+        #                  ('Rule 2', 'Short'): strategy.rule_2_short_stop
+        #                  }
+        #
 
-        for rule in list_of_enters:
+        for rule, side in list_of_enters:
             for x_last_row in range(-10,0):
-                if list_of_enters[rule](x_last_row) != True:
+                if list_of_enters[(rule,side)](x_last_row) != True:
                     continue
                 for remaining_row in range(x_last_row + 1, 0):
-                    if list_of_exits[rule](remaining_row) == True:
-                        return False, None, None
-                return True, x_last_row, str(rule)
-        return False, None, None
+                    if list_of_exits[(rule, side)](remaining_row) == True:
+                        return False, None, None, None
+                return True, x_last_row, rule, side
+        return False, None, None, None
 
-    def screen(self, trader, max_candle_history=10):
-        cursor = self.loader.sql.conn.cursor()
-        strategy = FabStrategy()
-        """ Add 'side when setting index"""
-        df_metrics = self.loader.sql.SELECT(f"* from metrics order by symbol", cursor).set_index(['symbol', 'tf', 'rule_no', 'side', 'metric_id']).astype(float)
+    def screen(self, trader, metrics_table, max_candle_history=10):
+        metrics_table = metrics_table[:]
+        symbols = pd.DataFrame(metrics_table.index.unique(level=0), columns=['symbol'])
+        tfs = pd.DataFrame(metrics_table.index.unique(level=1), columns=['tf'])
+        tfs = tfs.set_index('tf').drop([7, 21, 77, 240, 60]).reset_index()
+        df_symbol_tf = symbols.merge(tfs, how='cross')
+        max_candles_needed = [231+max_candle_history+1 for _ in range(len(df_symbol_tf))]
+
+        # return df_symbol_tf
         partial_screener = pd.DataFrame(columns=['date', 'signal', 'how recent', 'metric_id'])
-        master_screener = partial_screener
-        current_symbol, current_tf = None, None
-        signal = False
+
+        # Grabs Binance data for every single (symbol, tf) pair
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            results = executor.map(trader.set_asset, df_symbol_tf['symbol'].values, df_symbol_tf['tf'].values, max_candles_needed)
+            executor.shutdown(wait=True)
+
+        # return results
+
+        for df, symbol, tf in zip(results, df_symbol_tf['symbol'], df_symbol_tf['tf']):
+            signal, x_many_candles_ago, rule, side = self.check_for_signals(df, self.strategy, max_candle_history)
+
+            if signal:
+                # tf = int((df['date'].iloc[1]-df['date'].iloc[0]).total_seconds()/60)
+                metric_id = metrics_table.loc[(symbol, tf, rule, side), 'metric_id']
+                date = datetime.now() - timedelta(minutes=int(abs(x_many_candles_ago)*tf))
+                change_factor = float(df['close'].iloc[-1]/df['close'].iloc[x_many_candles_ago])
+                percentage_change = Helper.factor_to_percentage([change_factor])[0]
+                partial_screener = partial_screener.append(pd.DataFrame([[metric_id, date, str((rule, side)), x_many_candles_ago, percentage_change]],
+                                                    columns=['metric_id', 'date', 'signal', 'how recent', "% change"]))
+
+        master_screener = partial_screener.merge(metrics_table.reset_index()
+                                                      [['metric_id', 'symbol', 'tf', 'average_win', 'unrealized_rrr',
+                                                        'peak_unrealized_profit', 'num_candles_to_peak',
+                                                        'amount_of_data']],
+                                                      on='metric_id')
+
+        master_screener['most recent'] = master_screener.groupby('symbol')['how recent'].transform('max')
+        master_screener = master_screener.set_index(['symbol', 'tf']).sort_values(
+            ['most recent', 'symbol', 'how recent', 'unrealized_rrr'], ascending=[False, False, False, False])
+        master_screener = master_screener[['metric_id'] + list(master_screener.columns.drop('metric_id'))]
+        return master_screener
+
+    def screen_monitor(self, trader, minutes_to_sleep=1):
+        cursor = self.loader.sql.conn.cursor()
+        df_metrics = self.loader.sql.SELECT(f"* from metrics order by symbol", cursor).set_index(
+            ['symbol', 'tf', 'rule_no', 'side']).astype(float)
         count = 0
 
+        now = datetime.now().minute
         while True:
-            count += 1
-            for symbol, tf, rules, side, metric_id in df_metrics.index[:]:
-                if tf in [7, 21,77, 1, 3, 5]:
-                    continue
-                if symbol == current_symbol and tf == current_tf:
-                    continue
+            if datetime.now().minute == now + 1:
+                count += 1
+                print(datetime.now())
+                now = datetime.now().minute
+                master_screener = self.screen(trader=trader, metrics_table=df_metrics)
+                clear_output(wait=True)
+                if count > 1 and len(master_screener) != len(self.master_screener):
+                    new_values = []
+                    temp = pd.concat([master_screener, self.master_screener]).drop_duplicates(subset='metric_id', keep=False)
+                    for symbol, tf in temp.index:
+                        try:
+                            if temp.loc[(symbol, tf), 'how recent'] == -1:
+                                new_values.append((symbol, tf))
+                        except Exception():
+                            print(temp, symbol, tf)
+                    if len(new_values) > 0:
+                        # playsound(r"C:\Users\haseab\Desktop\Python\PycharmProjects\FAB\local\cash_register.mp3")
+                        print("new values added: ")
+                        print(new_values)
 
-                if (symbol, tf) in master_screener.index:
-                    if (symbol, tf) in self.master_screener.index:
-                        now = datetime.now()
-                        signal_date = self.master_screener.loc[(symbol, tf), 'date']
-                        self.master_screener.loc[(symbol, tf), 'how recent'] = -int((now - signal_date).total_seconds()/(60*tf))
-                        self.master_screener['most recent'] = master_screener.groupby('symbol')['how recent'].transform(
-                            'max')
-                        self.master_screener = self.master_screener.sort_values(
-                                                            ['most recent', 'symbol', 'how recent', 'unrealized_rrr'],
-                                                            ascending=[False, False, False, False])
-                        if self.master_screener.loc[(symbol, tf), 'how recent'] <-10:
-                            self.master_screener.drop((symbol, tf), inplace=True)
-                    continue
+                self.master_screener = master_screener[master_screener['amount_of_data'] >= 5].copy()
 
-                # print(f'checking: {symbol, tf} ')
-                df = trader.set_asset(symbol, tf, max_candles_needed=231 + max_candle_history + 1, drop_last_row=False)
+                display(self.master_screener)
 
-                signal, x_many_candles_ago, rule = self.check_for_signals(df, strategy, max_candle_history)
-                current_symbol, current_tf = symbol, tf
+                time.sleep(50*minutes_to_sleep)
+            time.sleep(0.5)
 
-                if signal:
-                    date = datetime.now() - timedelta(minutes=abs(x_many_candles_ago)*tf)
-                    partial_screener = partial_screener.append(pd.DataFrame([[date, rule, x_many_candles_ago, metric_id]],
-                                                        columns=['date', 'signal', 'how recent', 'metric_id']))
-                    signal, x_many_candles_ago= None, None
-                    master_screener = partial_screener.merge(df_metrics.reset_index()
-                                                                  [['metric_id', 'symbol', 'tf', 'unrealized_rrr',
-                                                                    'peak_unrealized_profit', 'num_candles_to_peak',
-                                                                    'amount_of_data']],
-                                                                  on='metric_id')
-                    master_screener['most recent'] = master_screener.groupby('symbol')['how recent'].transform('max')
-                    master_screener = master_screener.set_index(['symbol', 'tf']).sort_values(
-                        ['most recent', 'symbol', 'how recent', 'unrealized_rrr'], ascending=[False, False, False, False])
-                    self.master_screener = master_screener[master_screener['amount_of_data'] >= 10].copy()
-
-                    if count > 1 and (symbol, tf) in self.master_screener:
-                        print(f'new value added: {symbol, tf}')
-                        playsound(r"C:\Users\haseab\Desktop\Python\PycharmProjects\FAB\local\cash_register.mp3")
-
-                    yield self.master_screener
-            yield self.master_screener
-
-        # return self.master_screener.set_index(['symbol','tf']).sort_values("unrealized_rrr", ascending=False)
-
-    def screen_monitor(self, trader):
-        screen_generator = self.screen(trader=trader)
-        while True:
-            clear_output(wait=True)
-            try:
-                display(next(screen_generator))
-            except StopIteration:
-                return self.master_screener
 
 
 
