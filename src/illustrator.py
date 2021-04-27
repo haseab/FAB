5619ab4d1d18a5fc38c91a6831de97d5b2b1b64b
haseab <hullah3000@gmail.com>
2021-04-26 23:28:40 -0400

    Refactor illustration of data

--- src/illustrator.py
+++ src/illustrator.py
@@ -6,7 +6,6 @@ from helper import Helper
 import numpy as np
 from datetime import timedelta
 
-
 class Illustrator:
     """
     WORK IN PROGRESS - Responsible for analysis of trading history.
@@ -23,42 +22,13 @@ class Illustrator:
     def __init__(self):
         self.strategy = FabStrategy()
 
-    def show_trade_graph(self, df_th, df_tf_candles, tid, save=False, adjust_left_view=29, adjust_right_view=10):
-        start_datetime = df_th.loc[tid, 'enter_date']
-        end_datetime = df_th.loc[tid, 'exit_date']
-        enter_price = df_th.loc[tid, 'enter_price']
-        exit_price = df_th.loc[tid, 'exit_price']
-        position_side = df_th.loc[tid, 'side']
-        tf = df_th['tf'].iloc[0]
-
-        pre_data = df_tf_candles.loc[start_datetime - pd.Timedelta((231+adjust_left_view) * tf, 'minutes'): start_datetime -pd.Timedelta(1 * tf, 'minutes'), :].copy()
-        post_data = df_tf_candles.loc[end_datetime + pd.Timedelta(1 * tf, 'minutes'): end_datetime + pd.Timedelta(adjust_right_view * tf, 'minutes'), :].copy()
-
-        trade_data = df_tf_candles.loc[start_datetime:end_datetime, :].copy()
-        trade_data['pnl_line'] = np.linspace(enter_price, exit_price, len(trade_data))
-        illus_data = pre_data.append(trade_data.append(post_data))
-
-        return self.graph_trade_data(illus_data, position_side, tid, save)
-
     def download_all_trades(self, df_trading_history, df_tf_candles, save=False):
         df_tf_candles = df_tf_candles.reset_index().set_index('date')
         df_th = df_trading_history.reset_index().set_index('tid')
         for tid in df_th.index:
-            self.show_trade_graph(df_th, df_tf_candles, tid, save)
+            self.prepare_trade_graph_data(df_th, df_tf_candles, tid, save)
             print('saved file')
         return "Saved all of them"
-
-    def graph_df(self, df_graph, sma=True, space=True):
-        if df_graph.index.name != 'date':
-            df_graph = df_graph.reset_index().set_index('date')
-        try:
-            df_graph['green']
-        except KeyError:
-            df_graph = self.add_sma_to_df(df_graph)
-        df_graph, addplot = self.set_graph_style(df_graph, sma=sma)
-
-        graph = mpf.plot(df_graph, type='candle', figratio=(40, 15), datetime_format='%Y-%m-%d %H:%M:%S',
-                         tight_layout=True, xrotation=5, xlim=(0, len(df_graph)+50), style=self.haseab_style, addplot=addplot)
 
     def set_graph_style(self, df_graph, sma=True):
         addplot = []
@@ -70,7 +40,9 @@ class Illustrator:
             ma_orange = mpf.make_addplot(df_graph['orange'], color='#ff7300', width=2.0)
             ma_blue = mpf.make_addplot(df_graph['blue'], color='#001aff', width=2.0)
             ma_black = mpf.make_addplot(df_graph['black'], color='#000000', width=2.0)
-            addplot = [ma_green, ma_orange, ma_blue, ma_black]
+            ma_light_blue = mpf.make_addplot(df_graph['light blue'], color="#8cf5ff", width=2.0)
+            # ma_red = mpf.make_addplot(df_graph['red'], color='red', width=2.0)
+            addplot = [ma_green, ma_orange, ma_blue, ma_black, ma_light_blue]
         return df_graph, addplot
 
     def add_sma_to_df(self, df):
@@ -81,38 +53,72 @@ class Illustrator:
         df['orange'] = self.strategy.orange
         df['blue'] = self.strategy.blue
         df['black'] = self.strategy.black
+        df['light blue'] = self.strategy.light_blue
+        # df['red'] = self.strategy.red
+
         return df[231:]
 
-    def graph_trade_data(self, dataframe: pd.DataFrame, position_side, tid = None, save=False):
-        """Graphs the selected data on a wide chart
-        :return plot """
-        df_graph = dataframe.copy()
+    def prepare_trade_graph_data(self, df_th, df_tf_candles, tid, tf, data_only=False, flat=False, adjust_left_view=29, adjust_right_view=10):
+        start_datetime = df_th.loc[tid, 'enter_date']
+        end_datetime = df_th.loc[tid, 'exit_date']
+        enter_price = df_th.loc[tid, 'enter_price']
+        exit_price = df_th.loc[tid, 'exit_price']
+        position_side = df_th.loc[tid, 'side']
+        date_column = df_tf_candles['date']
+        base_tf = df_th['tf'].iloc[0]
 
-        df_graph = self.add_sma_to_df(df_graph)
+        adjusted_pre_data_start_datetime = start_datetime - pd.Timedelta((231 * tf) + adjust_left_view * tf, 'minutes')
+        adjusted_pre_data_end_datetime = start_datetime - pd.Timedelta(1 * tf, 'minutes')
+        adjusted_post_data_start_datetime = end_datetime + pd.Timedelta(1 * tf, 'minutes')
+        adjusted_post_data_end_datetime = end_datetime + pd.Timedelta(adjust_right_view * tf, 'minutes')
 
-        if save:
-            file_name = f"C:\\Users\\haseab\\Desktop\\Python\\PycharmProjects\\FAB\\local\\PNG Images\\BTCUSDT Trade No {tid}.png"
+        pre_data = df_tf_candles[date_column.between(adjusted_pre_data_start_datetime, adjusted_pre_data_end_datetime)].copy().reset_index().set_index('date')
+        post_data = df_tf_candles[date_column.between(adjusted_post_data_start_datetime, adjusted_post_data_end_datetime)].copy().reset_index().set_index('date')
 
-        pnl_line = df_graph['pnl_line'].dropna()
+        trade_data = df_tf_candles[date_column.between(start_datetime, end_datetime)].copy().reset_index().set_index('date')
+        trade_data['pnl_line'] = np.linspace(enter_price, exit_price, len(trade_data))
+        illus_data = pre_data.append(trade_data.append(post_data))
 
-        if len(pnl_line) == 1:
-            self.graph_df(df_graph, sma=True)
+        df_graph = self.add_sma_to_df(illus_data)
+        if len(df_graph) == 0:
+            return pd.DataFrame()
+
+        if data_only:
             return df_graph
 
-        price_diff = pnl_line[-1]-pnl_line[-2]
-        if (position_side == "Short" and price_diff<0) or (position_side == "Long" and price_diff>0):
-            profit_line_inner = mpf.make_addplot(df_graph['pnl_line'], color='#00ff26', marker='o', markersize=50, width=4)
-        else:
-            profit_line_inner = mpf.make_addplot(df_graph['pnl_line'], color='red', marker='o', markersize=50, width=4)
+        return self.graph_df(df_graph, position_side=position_side, flat=flat)
 
-        profit_line_outer = mpf.make_addplot(df_graph['pnl_line'], color='black', marker='o', markersize=50, width=7.0)
+    def graph_df(self, df_graph, position_side=None, sma=True, space=0, flat=False):
 
-        df_graph, addplot = self.set_graph_style(df_graph, sma=True)
+        if df_graph.index.name != 'date':
+            df_graph = df_graph.reset_index().set_index('date')
+        try:
+            df_graph['green']
+        except KeyError:
+            df_graph = self.add_sma_to_df(df_graph)
 
-        if save:
-            graph = mpf.plot(df_graph, type='candle', figratio=(40,15), datetime_format='%Y-%m-%d %H:%M:%S', tight_layout=True, xrotation =5,
-                             style=self.haseab_style, addplot=addplot + [profit_line_outer, profit_line_inner], savefig=file_name)
-        else:
-            graph = mpf.plot(df_graph, type='candle', figratio=(40,15), datetime_format='%Y-%m-%d %H:%M:%S', tight_layout=True, xrotation =5,
-                             style=self.haseab_style, addplot=addplot + [profit_line_outer, profit_line_inner])
-        return df_graph
+        df_graph, addplot = self.set_graph_style(df_graph, sma=sma)
+
+        figratio = (40,15)
+        if flat:
+            figratio = (40,10)
+
+        if position_side:
+            pnl_line = df_graph['pnl_line'].dropna()
+
+            if len(pnl_line) == 1:
+                self.graph_df(df_graph, sma=True)
+                return df_graph
+
+            price_diff = pnl_line[-1]-pnl_line[-2]
+            if (position_side == "Short" and price_diff<0) or (position_side == "Long" and price_diff>0):
+                profit_line_inner = mpf.make_addplot(df_graph['pnl_line'], color='#00ff26', marker='o', markersize=50, width=4)
+            else:
+                profit_line_inner = mpf.make_addplot(df_graph['pnl_line'], color='red', marker='o', markersize=50, width=4)
+            profit_line_outer = mpf.make_addplot(df_graph['pnl_line'], color='black', marker='o', markersize=50, width=7.0)
+
+            addplot += [profit_line_outer, profit_line_inner]
+
+        graph = mpf.plot(df_graph, type='candle', figratio=figratio, datetime_format='%Y-%m-%d %H:%M:%S',
+                         tight_layout=True, xrotation=180, xlim=(0, len(df_graph)+space), style=self.haseab_style,
+                         warn_too_much_data=10000, addplot=addplot)
